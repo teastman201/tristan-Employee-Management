7da89a20e4d21596f8032ee9d306541ac981f0b6
Tristan Eastman <teastman85@gmail.com>
2020-10-10 07:24:33 -0400

    Add start function and prompts.

--- employeeManagementCRUD.js
+++ employeeManagementCRUD.js
@@ -1,6 +1,7 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -11,6 +12,55 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-
+  startApp();
 });
+
+function startApp() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "Add department",
+        "Add roles",
+        "Add employees",
+        "View departments",
+        "View roles",
+        "View employees",
+        "Update employees"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Add department":
+          departmentAdd();
+          break;
+
+        case "Add roles":
+          roleAdd();
+          break;
+
+        case "Add employees":
+          employeeAdd();
+          break;
+
+        case "View departments":
+          departmentView();
+          break;
+
+        case "View roles":
+          roleView();
+          break;
+
+        case "View employees":
+          employeeView();
+          break;
+
+        case "Update employees":
+          employeeUpdate();
+          break;
+      }
+    });
+}
 
